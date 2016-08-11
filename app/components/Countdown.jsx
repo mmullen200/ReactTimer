8ada0eb1bb6aa0f5e982aacb19b7167d029a92ce
Michael Mullen <michaeldmullen@gmail.com>
2016-08-11 08:45:15 -0700

    added Countdown tests

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -4,13 +4,40 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
 	getInitialState: function () {
-		return {count: 0};
+		return {
+			count: 0,
+			// This keeps track of the countdown state
+			// Can be started, stoppped, or paused. Default is stopped
+			countdownStatus: 'stopped'
+		};
 	},
 
+	// life cycle method. Gets called every time props or states are updated.
+	componentDidUpdate: function (prevProps, prevState) {
+		// Because this method will be called every time something updates,
+		// like, every time we count down a second, we add this if statement
+		// so the method will only be called in certain cases.
+		if (this.state.countdownStatus !== prevState.countdownStatus) {
+			switch (this.state.countdownStatus) {
+				case 'started':
+					this.startTimer();
+					break;
+			}
+		}
+	},
+	startTimer: function () {
+		this.timer = setInterval(() => {
+			var newCount = this.state.count - 1;
+			this.setState({
+				count: newCount >= 0 ? newCount : 0
+			});
+		}, 1000); 
+	},
 
 	handleSetCountdown: function (seconds) {
 		this.setState({
-			count: seconds
+			count: seconds,
+			countdownStatus: 'started'
 		});
 	},
 
