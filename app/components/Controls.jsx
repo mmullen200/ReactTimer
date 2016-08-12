5c2974ee0371888c81fb6879c421b09e5d0bd354
Michael Mullen <michaeldmullen@gmail.com>
2016-08-12 10:31:46 -0700

    added pause and stop buttons

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -4,8 +4,18 @@ var Controls = React.createClass({
 	propTypes: {
 		// The countdown status needs to be passed down to this component so
 		// the buttons can be dynamically generated.
-		countdownStatus: React.PropTypes.string.isRequired
+		countdownStatus: React.PropTypes.string.isRequired,
+		onStatusChange: React.PropTypes.func.isRequired
 	},
+	
+	// We'll create an onclick function that takes care of all the buttons
+	// instead of creating one for each button.
+	onStatusChange: function (newStatus) {
+		return () => {
+			this.props.onStatusChange(newStatus);
+		}
+	},
+
 	render: function () {
 		var {countdownStatus} = this.props;
 
@@ -13,16 +23,17 @@ var Controls = React.createClass({
 		// countdownStatus
 		var renderStartStopButton = () => {
 			if (countdownStatus === 'started') {
-				return <button className="button secondary">Pause</button>
+				return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
 			} else if (countdownStatus === 'paused') {
-				return <button className="button primary">Start</button>
+				return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
 			}
 		};
 
 		return (
 			<div className="controls">
 				{renderStartStopButton()}
-				<button className="button alert hollow">Clear</button>
+				
+				<button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
 			</div>
 			)
 	}
